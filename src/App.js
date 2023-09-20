b381acca34df8e25a21d152a1245dd2be06fdec4
donut <sunruyun2@gmail.com>
2023-09-20 17:06:47 +0100

    add side bar and navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router , Route, Redirect, Switch} from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
-  <Router>
-    <Switch>
-      <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new">
-          <NewPlace />
-        </Route>
-        <Redirect to="/"/>
-    </Switch>
-  </Router>
-  );  
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new">
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,24 +1,28 @@
 import React from "react";
+
 import './UsersList.css'
 import UserItem from './UserItem'
+import Card from '../../shared/components/UIElements/Card'
 
 const UserList = props => {
-    if (props.items.length === 0){
+    if (props.items.length === 0) {
         return <div className="className">
-            <h2>No users found.</h2>
+            <Card>
+                <h2>No users found.</h2>
+            </Card>
         </div>
-    } 
+    }
 
     return (
-    <ul className="users-list">
-        {props.items.map(user =>  (
-            <UserItem key={user.id} 
-                id = {user.id} 
-                image={user.image} 
-                name={user.name} 
-                placeCount = {user.places}
-            />))}
-    </ul>
+        <ul className="users-list">
+            {props.items.map(user => (
+                <UserItem key={user.id}
+                    id={user.id}
+                    image={user.image}
+                    name={user.name}
+                    placeCount={user.places}
+                />))}
+        </ul>
     )
 }
 
