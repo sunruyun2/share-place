b229cc4b4c2ef078ba53870e70002cf7777487fe
donut <sunruyun2@gmail.com>
2023-09-29 11:36:23 +0100

    neat display of user places, using dynamic user segment

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
+import PlaceList from './places/components/PlaceList';
 
 const App = () => {
   return (
@@ -13,6 +15,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new">
             <NewPlace />
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,7 +1,37 @@
 import React from "react";
 
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+    {
+        id:'p1',
+        title:'Empire State Building',
+        description: 'One of the greatest sky scrapers in the world!',
+        imageUrl: 'https://images.pexels.com/photos/2019546/pexels-photo-2019546.jpeg',
+        address: '20 W 34th St., New York, NY 10001, United States',
+        location: {
+            lat:40.7484445,
+            lng:-73.9882393,
+        },
+        creator: 'u1'
+    },
+    {
+        id:'p2',
+        title:'Empire State Building',
+        description: 'One of the greatest sky scrapers in the world!',
+        imageUrl: 'https://images.pexels.com/photos/2019546/pexels-photo-2019546.jpeg',
+        address: '20 W 34th St., New York, NY 10001, United States',
+        location: {
+            lat:40.7484445,
+            lng:-73.9882393,
+        },
+        creator: 'u2',
+    },
+]
+
+
 const UserPlaces = () =>{
-    return;
+    return<PlaceList items={DUMMY_PLACES} />
 }
 
 export default UserPlaces;
