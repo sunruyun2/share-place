abd968b0dff202cffaf544899a0b74ef7569308b
donut <sunruyun2@gmail.com>
2023-10-06 20:31:46 +0100

    manage state in the input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -5,7 +5,7 @@ import Input from "../../shared/components/FormElements/Input";
 
 const NewPlace = () => {
     return <form className="place-form">
-        <Input type="text" label="Title" element="input"/>
+        <Input type="text" label="Title" element="input" validators={[]} errorText="please enter a valid title"/>
     </form>
 }
 
