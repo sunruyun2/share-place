e8309c960dc9462a29cec5a023ae843954d3c403
donut <sunruyun2@gmail.com>
2023-11-04 17:35:10 +0000

    use fetch to send http request from frontend to backend

--- src/places/pages/Auth.js
+++ src/places/pages/Auth.js
@@ -27,9 +27,32 @@ const Auth = () =>{
     },false)
 
 
-    const authSubmitHandler = event => {
+    const authSubmitHandler = async event => {
         event.preventDefault();
-        console.log(formState.inputs);
+    
+        //send http request to backend
+        if (isLoginMode){
+
+        } else {
+            try {
+                const response = await fetch('http://localhost:5000/api/users/signup', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        name:formState.inputs.name.value,
+                        email:formState.inputs.email.value,
+                        password:formState.inputs.email.password,
+                    })
+                });
+
+                const responseData = await response.json();
+                console.log(responseData)
+            } catch (e) {
+                console.log(e)
+            }
+        }
         auth.login();
     }
 
