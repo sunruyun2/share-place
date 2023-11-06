4ccc5f973d17992d551375760664593c122db086
donut <sunruyun2@gmail.com>
2023-11-06 11:38:29 +0000

    sending a login request

--- src/places/pages/Auth.js
+++ src/places/pages/Auth.js
@@ -25,19 +25,43 @@ const Auth = () => {
             value: '',
             isValid: false
         }
-
+        
     }, false)
-
-
+    
+    
     const authSubmitHandler = async event => {
         event.preventDefault();
-
+        
+        setIsLoading(true);
         //send http request to backend
         if (isLoginMode) {
+            try {
+                const response = await fetch('http://localhost:5000/api/users/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        email: formState.inputs.email.value,
+                        password: formState.inputs.password.value,
+                    })
+                });
+
+                const responseData = await response.json();
+                if (!response.ok) {
+                    throw new Error(responseData.message)
+                }
+
+                setIsLoading(false)
+                auth.login();
+            } catch (e) {
+                console.log(e)
+                setIsLoading(false)
+                setError(e.message || 'Something went wrong, please try again')
+            }
 
         } else {
             try {
-                setIsLoading(true);
                 const response = await fetch('http://localhost:5000/api/users/signup', {
                     method: 'POST',
                     headers: {
