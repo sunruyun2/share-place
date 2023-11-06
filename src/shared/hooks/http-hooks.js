5431a4f8ad3e89ce5fa38359491a08ebd951dbbb
donut <sunruyun2@gmail.com>
2023-11-06 12:39:13 +0000

    create http hook

---
+++ src/shared/hooks/http-hooks.js
@@ -0,0 +1,45 @@
+import { useState , useCallback, useRef, useEffect} from "react";
+export const useHttpClient = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+
+    const activeHttpRequests = useRef([]);
+
+    const sendRequest = useCallback( async(url, method = 'GET', body = null, headers = {}) =>{
+        setIsLoading(true)
+        const httpAbortCtrll =  new AbortController();
+        activeHttpRequests.current.push(httpAbortCtrll);
+
+        try {
+            const response = await fetch(url, {
+                method,
+                body,
+                headers,
+                signal: httpAbortCtrll.signal
+            })
+    
+            const responseData = await response.jason();
+            if(!response.ok){
+                throw new Error(responseData.message)
+            }
+
+            return responseData
+        } catch (e) {
+            setError(e.message)
+        }
+        
+        setIsLoading(false)
+    }, [])
+
+    const clearError = () => {
+        setError(null)
+    }
+
+    useEffect( ()=>{
+        return () => {
+            activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort())
+        }
+    },[] )
+
+    return { isLoading, error, sendRequest, clearError};
+};
