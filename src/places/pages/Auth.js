0622b5dbb04d1a03b7d3cafacd57b5b841f0cd25
donut <sunruyun2@gmail.com>
2023-11-10 12:15:36 +0000

    use formData to store image

--- src/places/pages/Auth.js
+++ src/places/pages/Auth.js
@@ -34,8 +34,6 @@ const Auth = () => {
     const authSubmitHandler = async event => {
         event.preventDefault();
 
-        console.log(formState.inputs)
-        
         //send http request to backend
         if (isLoginMode) {
             try {
@@ -56,20 +54,20 @@ const Auth = () => {
 
         } else {
             try {
+                const formData = new FormData();
+                formData.append('email' , formState.inputs.email.value)
+                formData.append('name' , formState.inputs.name.value)
+                formData.append('password', formState.inputs.password.value)
+                formData.append('image', formState.inputs.image.value)
+
                 const responseData = await sendRequest('http://localhost:5000/api/users/signup',
                     'POST',
-                    JSON.stringify({
-                        name: formState.inputs.name.value,
-                        email: formState.inputs.email.value,
-                        password: formState.inputs.password.value,
-                    }),
-                    {
-                        'Content-Type': 'application/json',
-                    },    
+                    formData
                 );
                 auth.login(responseData.user.id);
+                console.log("form data", formData)
             } catch (e) {
-
+                console.log("error:" + e)
             }
         }
     }
@@ -115,7 +113,8 @@ const Auth = () => {
                             onInput={inputHandler}
                         />
                     )}
-                    {!isLoginMode && <ImageUpload center id="image" onInput={inputHandler}/>}
+                    {!isLoginMode && 
+                        (<ImageUpload center id="image" onInput={inputHandler}/>)}
                     <Input
                         id="email"
                         element="input"
